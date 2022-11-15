3c4ecee0b7c97e3b61b3a1246540440980bb8325
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-11-15 16:09:35 +0530

    py

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+person = {'name':'param patel','age':18,'weight':45.45,'gen':True}
+# print(person['name'])
+# print(person['gen'])
+# print(person)
+# person['name']='param patel'
+# print(person)
+# person['name'] = ['param patel','dhaivat','ghanshyambhai',{'value':10,"value2":30}]
+# print("data is here",person)
+# tamp=person.items()
+# print(tamp)
+# temp=person.keys()
+# print(tamp)
+
+# dic={'value':30,'values':14}
+
+temp=person.keys()
+print(temp)
---
+++ list.py
@@ -0,0 +1,10 @@
+#let list create 
+frutis =['mego','banana','apple']
+print(frutis)
+print(frutis[2])
+frutis[2]='chiku'  
+vagi=['cab','jamrukh']
+print(vagi)
+print(frutis+vagi)
+print(frutis[0:2])
+print(frutis*5)
