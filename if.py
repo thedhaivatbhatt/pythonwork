f272193764e92235f851d17e72c306bc44ff82f7
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-11-17 14:43:59 +0530

    if

---
+++ if.py
@@ -0,0 +1,7 @@
+# write a program to accept age from user in years. display message "you are eligible for voting" if age is above or equal to 18
+
+year=int(input("entar value of num"))
+
+if year>=18:
+    print("you are eligible for voting")
+e
---
+++ try.py
@@ -0,0 +1,9 @@
+a=30
+b=30
+
+ 
+if a==b:
+    print("both are equel")  
+else:
+    print("a and b is not equel")
+
