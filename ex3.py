bc546e87fe1f02f54a278d6256236b1700e5b0b6
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-11-22 14:52:48 +0530

    py

---
+++ ex3.py
@@ -0,0 +1,12 @@
+# write a calculateter 
+num1=int(input("num1 - "))
+num2=int(input("num2 - "))
+cal=num1+num2
+cal1=num1-num2
+cal2=num1*num2
+cal3=num1/num2
+print("add",cal)
+print("sub",cal1)
+print("mul",cal2)
+print("div",cal3)
+
